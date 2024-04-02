a8a064ac5a9fed37c072481eab7cbb41aecd5ecf
Murillo Ribeiro <murilloribeiro.contato@gmail.com>
2024-04-02 14:38:18 -0300

    ForEach

---
+++ src/BasicsJava/ForEach.java
@@ -0,0 +1,15 @@
+package BasicsJava;
+
+public class ForEach {
+
+    public static void main(String[] args) {
+
+        String[] animals = {"cat", "dog", "bird"};
+
+        for(String i : animals) {
+            System.out.println(i);
+        }
+
+    }
+
+}
