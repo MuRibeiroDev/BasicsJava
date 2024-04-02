e6b5411d4c6ebbe6dc6bdd530b2e1434745a8186
Murillo Ribeiro <murilloribeiro.contato@gmail.com>
2024-04-02 17:09:42 -0300

    OverloadedMethods

---
+++ src/BasicsJava/OverloadedMethods.java
@@ -0,0 +1,33 @@
+package BasicsJava;
+
+public class OverloadedMethods {
+    public static void main(String[] args) {
+
+        // overloaded methods = methods that share the same name byt have different parameters
+        // method name + parameters = method signature
+
+        double x = add(24, 25);
+        // double y = add(24, 25, 29);
+        // double z = add(24, 25, 34, 10);
+
+        System.out.println(x);
+    }
+
+    static int add(int a, int b) {
+        System.out.println("This is overloaded method #1");
+        return a + b;
+    }
+
+    static int add(int a, int b, int c ) {
+        System.out.println("This is overloaded method #2");
+        return a + b + c;
+    }
+
+    static int add(int a, int b, int c, int d) {
+        System.out.println("This is overloaded method #3");
+        return a + b + c + d;
+    }
+
+
+
+}
