bde9717a92a750c7bfda3068437f658c814ab501
Murillo Ribeiro <murilloribeiro.contato@gmail.com>
2024-04-02 16:43:21 -0300

    Methods

--- src/BasicsJava/Methods.java
+++ src/BasicsJava/Methods.java
@@ -7,11 +7,26 @@ public class Methods {
 
         // Call the method name and set parameters that the function requires
         hello(name, 18);
+
+        int z = add(5, 6);
+
+        System.out.println(z);
+        // System.out.println(add(5, 6););
     }
 
     // the function hello have two parameters, and print hello + name + age
-    static void hello(String name, int age){
-        System.out.println("Hello "+ name + " " + age);
+    static void hello(String name, int age) {
+        System.out.println("Hello " + name + " " + age);
+    }
+
+    static int add(int x, int y) {
+
+        int z = x + y;
+
+        // return x + y
+
+        return z;
+
     }
 
 }
